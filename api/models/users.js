5897ff752ef1329b515c19a5cf794f734518fe41
brianwang9100@gmail.com <brianwang9100@gmail.com>
2018-01-17 22:45:54 -0500

    initial commit

---
+++ api/models/users.js
@@ -0,0 +1,53 @@
+'use strict';
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+var Schema = mongoose.Schema;
+
+
+var UsersSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    hash: String,
+    salt: String,
+
+    created_date: {
+        type: Date,
+        default: Date.now
+    },
+    // status: {
+    //     type: [{
+    //         type: String,
+    //         enum: ['pending', 'ongoing', 'completed']
+    //     }],
+    //     default: ['pending']
+    // },
+});
+
+UsersSchema.methods.setPassword = function (password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+UsersSchema.methods.validPassword = function (password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+    return this.hash === hash;
+};
+
+UsersSchema.methods.generateJwt = function () {
+    var expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        name: this.name,
+        exp: parseInt(expiry.getTime() / 1000),
+    }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
+};
+
+module.exports = mongoose.model('User', UsersSchema);
