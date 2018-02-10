8c724cc00263d226595bd83982087744b3641d82
brianwang9100@gmail.com <brianwang9100@gmail.com>
2018-02-10 16:03:04 -0500

    changed cors impl, now working

--- src/app.js
+++ src/app.js
@@ -37,6 +37,15 @@ app.use(cookieParser());
 // [SH] Initialise Passport before using the route middleware
 app.use(passport.initialize());
 
+var options = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Origin', 'Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+}
+app.use(cors(options));
+routesApi.all('*', cors(options));
 // [SH] Use the API routes when path starts with /api
 app.use('/api', routesApi);
 
@@ -45,14 +54,19 @@ app.use('/api', routesApi);
 // app.use(function (req, res) {
 //     res.sendFile(path.join(__dirname, 'app_client', 'index.html'));
 // });
-var options = {
-    origin: "*",
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type','Authorization'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-}
-app.use(cors(options));
+
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
+app.get('/', function (req, res, next) {
+    res.status(200).json({
+        message: "JOHN ARE U IN"
+    });
+    next();
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
