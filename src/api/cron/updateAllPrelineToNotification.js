74b2540992db5896fdfe1a8be1664f7a2767e78f
Jesse Hayes <jhayes@gatech.edu>
2018-03-07 15:51:17 -0500

    rename variables again lol

--- src/api/cron/updateAllPrelineToNotification.js
+++ src/api/cron/updateAllPrelineToNotification.js
@@ -11,10 +11,10 @@ function job() {
     // console.log('LineManager: updating preline to notification')
     // counts how many users are in Notification(N) or Inline(IL)
     // N + IL <= BATCH_SIZE
-    var countNotifAndInline = {};
+    var notifAndInlineLines = {};
 
     // keeps track of lines to notify per employer
-    var countPreline = {};
+    var prelineLines = {};
 
     // query to count all notification and inline, grouped by employer_id
     // i can probably replace this with count and group by.
@@ -25,10 +25,10 @@ function job() {
             return console.log("LineManagerError: " + err);
         // count up lines by employer
         lines.forEach( function(line) {
-            if (countNotifAndInline[line.employer_id]) {
-                countNotifAndInline[line.employer_id]++;
+            if (notifAndInlineLines[line.employer_id]) {
+                notifAndInlineLines[line.employer_id]++;
             } else {
-                countNotifAndInline[line.employer_id] = 1;
+                notifAndInlineLines[line.employer_id] = 1;
             }
         });
 
@@ -43,24 +43,24 @@ function job() {
             // keep track of prelines to update to notification
             lines.forEach( function(line) {
                 // if it exists
-                if (countPreline[line.employer_id]) {
+                if (prelineLines[line.employer_id]) {
                     // add to the array, as long as selected PL + N + IL < BATCH_SIZE
-                    if ((countPreline[line.employer_id].length + countNotifAndInline[line.employer_id] || 0) < BATCH_SIZE) {
-                        countPreline[line.employer_id].push(line);
+                    if ((prelineLines[line.employer_id].length + notifAndInlineLines[line.employer_id] || 0) < BATCH_SIZE) {
+                        prelineLines[line.employer_id].push(line);
                     }
                 } else {
                     // otherwise create the array, as long as N + IL is not maxed out.
-                    if ((countNotifAndInline[line.employer_id] || 0) < BATCH_SIZE) {
-                        countPreline[line.employer_id] = [line];
+                    if ((notifAndInlineLines[line.employer_id] || 0) < BATCH_SIZE) {
+                        prelineLines[line.employer_id] = [line];
                     } 
                 }
             });
         }).then(function(){
-            // console.log(countNotifAndInline)
-            // console.log(countPreline)
+            // console.log(notifAndInlineLines)
+            // console.log(prelineLines)
             // for each line item, change from preline to notificatio and notify the user.
-            for (var key in countPreline) {
-                var lines = countPreline[key];
+            for (var key in prelineLines) {
+                var lines = prelineLines[key];
                 lines.forEach( async function(line) {
                     let msg = await line.updateStatus("notification")
                     // send email notification to student to signal change.
